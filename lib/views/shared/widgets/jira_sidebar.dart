f66c8e452b852aa2e2337575a4abdeb1a36f599d
zaynrix <zayns1914@gmail.com>
2025-11-29 20:54:56 +0100

    Fix notification stream builder: Add proper type handling and null checks

--- lib/views/shared/widgets/jira_sidebar.dart
+++ lib/views/shared/widgets/jira_sidebar.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import '../../../config/constants/app_colors.dart';
 import '../../../services/interfaces/i_task_service.dart';
 import '../../../services/interfaces/i_actor_service.dart';
+import '../../../services/interfaces/i_notification_service.dart';
 import '../../actor/widgets/create_task_dialog.dart';
 import 'notification_center.dart';
 import 'notification_badge.dart';
